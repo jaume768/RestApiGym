88b76bdb865530c8b2f9ee9ae6f1772ff1459de3
jaume768 <jaumefernandezsunyer10@gmail.com>
2022-11-10 18:39:09 +0100

    push

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = express()
 app.use('/api',entrenosRutes)
 
 app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers','Content-Type');
+})
+
+app.use((req,res,next) => {
     res.status(404).json({
         message: 'Not found'
     })
