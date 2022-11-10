dafd6bfd060700eb2a8e3319345516f89a7ffefb
jaume768 <jaumefernandezsunyer10@gmail.com>
2022-11-10 18:48:26 +0100

    push

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const app = express()
 const cors = require('cors')
 
 app.use('/api',entrenosRutes)
-app.use(cors())
+app.use(cors({
+    origin: 'http://127.0.0.1:5500'
+}))
 
 app.use((req,res,next) => {
     res.status(404).json({
